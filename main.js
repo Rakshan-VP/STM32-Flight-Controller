7fbeb0fb47602c77d5d85fe85f91739dd942caf6
Rakshan V P <136898779+Rakshan-VP@users.noreply.github.com>
2025-05-23 11:57:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,41 +4,51 @@ let running = false;
 async function startSim() {
   if (running) return;
   running = true;
+
   let pyodide = await pyodideReadyPromise;
   await pyodide.loadPackage("numpy");
-  let response = await fetch("fc.py");
+  const response = await fetch("fc.py");
   await pyodide.runPythonAsync(await response.text());
 
-  let rpyPlot = {x: [], y: [[], [], [], []], name: ['Roll', 'Pitch', 'Yaw', 'Thrust']};
-  let motorPlot = {x: [], y: [[], [], [], []], name: ['M1', 'M2', 'M3', 'M4']};
-  let path = {x: [], y: []};
+  const rpyNames = ['Roll', 'Pitch', 'Yaw', 'Thrust'];
+  const motorNames = ['M1', 'M2', 'M3', 'M4'];
   let t = 0;
 
-  Plotly.newPlot('rpy_plot', [0,1,2,3].map(i => ({x:[], y:[], name:rpyPlot.name[i]})));
-  Plotly.newPlot('motor_plot', [0,1,2,3].map(i => ({x:[], y:[], name:motorPlot.name[i]})));
-  Plotly.newPlot('path', [{x:[], y:[], mode:'lines+markers'}]);
+  Plotly.newPlot('rpy_plot', rpyNames.map(n => ({x: [], y: [], name: n})));
+  Plotly.newPlot('motor_plot', motorNames.map(n => ({x: [], y: [], name: n})));
+  Plotly.newPlot('path', [{x: [], y: [], mode: 'lines+markers'}]);
 
   function loop() {
     if (!running) return;
-    let r = +roll.value, p = +pitch.value, y = +yaw.value, T = +thrust.value;
+    const r = +document.getElementById("roll").value;
+    const p = +document.getElementById("pitch").value;
+    const y = +document.getElementById("yaw").value;
+    const T = +document.getElementById("thrust").value;
+
     pyodide.globals.set("r", r);
     pyodide.globals.set("p", p);
     pyodide.globals.set("y", y);
     pyodide.globals.set("T", T);
-    pyodide.runPython("rpy_pid, motors = fc_step(r, p, y, T)");
 
-    let rpyt = pyodide.globals.get("rpy_pid").toJs();
-    let motors = pyodide.globals.get("motors").toJs();
+    pyodide.runPython("rpy_pid, motors = fc_step(r, p, y, T)");
+    const rpyt = pyodide.globals.get("rpy_pid").toJs();
+    const motors = pyodide.globals.get("motors").toJs();
+
     for (let i = 0; i < 4; i++) {
       Plotly.extendTraces('rpy_plot', {x:[[t]], y:[[rpyt[i]]]}, [i]);
       Plotly.extendTraces('motor_plot', {x:[[t]], y:[[motors[i]]]}, [i]);
     }
-    path.x.push(t);
-    path.y.push(rpyt[3]);
-    Plotly.update('path', {x: [path.x], y: [path.y]});
+
+    Plotly.update('path', {
+      x: [[...Array(t*10+1).keys()].map(x => x/10)],
+      y: [[...Array(t*10+1).keys()].map(i => rpyt[3])]]
+    });
+
     t += 0.1;
     setTimeout(loop, 100);
   }
 
   loop();
 }
+
+document.getElementById("startBtn").addEventListener("click", startSim);
