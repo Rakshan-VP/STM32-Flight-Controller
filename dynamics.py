e5a6d910ebd4bc0ceef8be0361cc3870a0a83bd3
Rakshan V P <136898779+Rakshan-VP@users.noreply.github.com>
2025-05-23 16:40:22 +0530

    Update dynamics.py

--- dynamics.py
+++ dynamics.py
@@ -1,21 +1,16 @@
-def motor_to_rpyt(m1, m2, m3, m4, T_max, l, I_x, I_y, I_z, k_yaw):
-    # Convert motor commands to thrusts
+def motor_to_rpyt(m1, m2, m3, m4, T_max, max_roll, max_pitch, max_yaw):
+    # Thrusts from normalized motor commands
     T1 = m1 * T_max
     T2 = m2 * T_max
     T3 = m3 * T_max
     T4 = m4 * T_max
 
     # Total thrust
-    thrust = T1 + T2 + T3 + T4  # in Newtons
+    thrust = T1 + T2 + T3 + T4
 
-    # Torques
-    tau_x = l * (T2 + T3 - T1 - T4)  # roll
-    tau_y = l * (T1 + T2 - T3 - T4)  # pitch
-    tau_z = k_yaw * (-T1 + T2 - T3 + T4)  # yaw
-
-    # Angular accelerations (simplified model)
-    roll = tau_x / I_x
-    pitch = tau_y / I_y
-    yaw = tau_z / I_z
+    # RPY angular accelerations (scaled)
+    roll  = (m2 + m3 - m1 - m4) * max_roll
+    pitch = (m1 + m2 - m3 - m4) * max_pitch
+    yaw   = (-m1 + m2 - m3 + m4) * max_yaw
 
     return roll, pitch, yaw, thrust
