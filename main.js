00952d48a7361e328140ce93bdb62f90a0993b29
Rakshan V P <136898779+Rakshan-VP@users.noreply.github.com>
2025-05-23 12:04:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,13 +10,26 @@ async function startSim() {
   const response = await fetch("fc.py");
   await pyodide.runPythonAsync(await response.text());
 
-  const rpyNames = ['Roll', 'Pitch', 'Yaw', 'Thrust'];
-  const motorNames = ['M1', 'M2', 'M3', 'M4'];
   let t = 0;
+  let pathX = [], pathY = [];
 
-  Plotly.newPlot('rpy_plot', rpyNames.map(n => ({x: [], y: [], name: n})));
-  Plotly.newPlot('motor_plot', motorNames.map(n => ({x: [], y: [], name: n})));
-  Plotly.newPlot('path', [{x: [], y: [], mode: 'lines+markers'}]);
+  Plotly.newPlot('rpy_plot', [
+    {x: [], y: [], name: 'Roll_cmd'}, {x: [], y: [], name: 'Pitch_cmd'},
+    {x: [], y: [], name: 'Yaw_cmd'}, {x: [], y: [], name: 'Thrust_cmd'},
+    {x: [], y: [], name: 'Roll_pid'}, {x: [], y: [], name: 'Pitch_pid'},
+    {x: [], y: [], name: 'Yaw_pid'}, {x: [], y: [], name: 'Thrust_pid'},
+  ]);
+
+  Plotly.newPlot('motor_plot', [
+    {x: [], y: [], name: 'M1'}, {x: [], y: [], name: 'M2'},
+    {x: [], y: [], name: 'M3'}, {x: [], y: [], name: 'M4'},
+  ]);
+
+  Plotly.newPlot('path', [{
+    x: [], y: [], mode: 'lines+markers', name: 'Drone Path'
+  }], {
+    margin: {t:0}, xaxis: {title: 'Time'}, yaxis: {title: 'Thrust'}
+  });
 
   function loop() {
     if (!running) return;
@@ -31,18 +44,26 @@ async function startSim() {
     pyodide.globals.set("T", T);
 
     pyodide.runPython("rpy_pid, motors = fc_step(r, p, y, T)");
-    const rpyt = pyodide.globals.get("rpy_pid").toJs();
+    const rpyt_pid = pyodide.globals.get("rpy_pid").toJs();
     const motors = pyodide.globals.get("motors").toJs();
 
+    const rpyt_cmd = [r, p, y, T];
+
+    // Extend RPY plot: 4 command + 4 PID values
     for (let i = 0; i < 4; i++) {
-      Plotly.extendTraces('rpy_plot', {x:[[t]], y:[[rpyt[i]]]}, [i]);
-      Plotly.extendTraces('motor_plot', {x:[[t]], y:[[motors[i]]]}, [i]);
+      Plotly.extendTraces('rpy_plot', {x: [[t]], y: [[rpyt_cmd[i]]]}, [i]);
+      Plotly.extendTraces('rpy_plot', {x: [[t]], y: [[rpyt_pid[i]]]}, [i + 4]);
     }
 
-    Plotly.update('path', {
-      x: [[...Array(t*10+1).keys()].map(x => x/10)],
-      y: [[...Array(t*10+1).keys()].map(i => rpyt[3])]]
-    });
+    // Extend Motor plot
+    for (let i = 0; i < 4; i++) {
+      Plotly.extendTraces('motor_plot', {x: [[t]], y: [[motors[i]]]}, [i]);
+    }
+
+    // Extend Path plot (use t for X and thrust for Y)
+    pathX.push(t);
+    pathY.push(rpyt_pid[3]);
+    Plotly.update('path', {x: [pathX], y: [pathY]});
 
     t += 0.1;
     setTimeout(loop, 100);
